d629a460f39b635b4f1e3da6d3875f6cba2a0f21
Mesconiji <josemesconi777@gmail.com>
2026-04-23 15:45:22 -0300

    agregado de JoseJSMesconiJose.js Jose Mesconi

---
+++ src/HolaJSMesconiJose.js
@@ -0,0 +1,12 @@
+//No usar VAR, usar LET o CONST
+
+var nombre = "Jose";
+
+let apellido = "Mesconi";
+//const TEXTO = "Hola mundo, según";
+const TEXTO = `Hola Mundo, Según ${nombre} ${apellido}`;
+
+
+//console.log("Hola Mundo, según" + nombre + apellido );
+
+console.log(TEXTO);
