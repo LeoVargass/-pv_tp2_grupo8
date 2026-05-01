099f105065cbc04b4cd2198426436ee55e1571c1
julieta ortega <jesme908@gmail.com>
2026-05-01 00:53:43 -0300

    Agrego autor Julieta Ortega en ejercicios 1, 7 y 13

--- src/js/ejercicio1.js
+++ src/js/ejercicio1.js
@@ -1,3 +1,4 @@
 const integrantes = "Julieta Ortega, Leornardo Vargas , Jose Mesconi, Santiago Urzagaste , Marcos Ovejero";
 
 console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript`);
+// Ejercicio 1 - Julieta Ortega
